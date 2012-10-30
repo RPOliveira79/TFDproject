40d38164e24e904748b605ae3280df5753c76ef3
Mateusz Ostafil <ratwolf0@gmail.com>
2012-10-30 12:29:01 +0000

    git test

--- UDPserver/src/Server/ServerTest.java
+++ UDPserver/src/Server/ServerTest.java
@@ -9,7 +9,8 @@ import javax.swing.JFrame;
 			int replicaId = 2; // to run replicas simply change this number and run this few times
 			UDPserver application1 = new UDPserver( replicaId ); // create server
 			//application1.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-
+			//hello git
+			
 			
 			//application.waitForPackets(); // run server application
 			} // end main
